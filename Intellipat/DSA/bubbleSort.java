821839d9a0f9781d11f97431ee3a67542aa8cfe1
Anil Kumar <mysteriousanil@gmail.com>
2023-11-03 02:17:17 +0530

    Update new File

---
+++ Intellipat/DSA/bubbleSort.java
@@ -0,0 +1,32 @@
+package oracle.src.DSA.Intellipat.DSA;
+
+public class bubbleSort {
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+int arr[] = {2,31,44,5,1,7};
+// Bubble sort
+for(int i=0; i<arr.length-1; i++){
+    for(int j=0; j<arr.length-i-1; j++){
+if(arr[j]>arr[j+1]){
+
+    //Swap
+    int temp = arr[j];
+    arr[j] = arr[j+1];
+    arr[j+1] = temp;
+}
+
+    }
+}
+
+
+printArray(arr);
+
+    }
+    
+}
